95309bcfa59d9c65424f7505c76fcc6813eb58df
yusuf1 <yusufsmik@gmail.com>
2023-05-21 20:31:53 +0800

    tambah character, tambah transition, dan betulkan question

--- js/script.js
+++ js/script.js
@@ -77,23 +77,20 @@ monogatari.assets ('scenes', {
 
 // Define the Characters
 monogatari.characters ({
-	'm': {
-		name: 'Madison',
+	'e': {
+		name: 'Emily',
 		color: '#5bcaff',
-		directory: 'saki',
+		directory: 'emily',
 		sprites: {
-			default: 'saki_normal.png',
-            angry: 'saki_angry.png',
-            curious: 'saki_curious.png',
-            happy: 'saki_happy.png',
-            suprised: 'saki_surprised.png',
-            worried: 'saki_worried.png'
+			default: '1_default.png',
+            talking: '1_talking.png',
+            
 		}
 	},
 	'a': {
 		name: 'Azri',
 		color: '#5bcaff', // what text color will be displayed
-		directory: 'yuki',
+		directory: 'azri',
 		sprites: {
 			default: '1.png',
 			smile: '2.png',
@@ -106,7 +103,29 @@ monogatari.characters ({
 		},
 		// nvl: true, //jadi mod nvl or 3rd person pov
 
-	}
+	},
+	'aa': {
+		name: 'Aaron',
+		color: '#5bcaff', // what text color will be displayed
+		directory: 'aaron',
+		sprites: {
+			default: '5_smile.png',
+			smile: '5_smile2.png',
+			
+		},
+
+	},
+	'li': {
+		name: 'Lisa',
+		color: '#5bcaff', // what text color will be displayed
+		directory: 'lisa',
+		sprites: {
+			default: '3_normal.png',
+			smile: '3_slight mad.png',
+			
+		},
+
+	},
 });
 
 const person = {
@@ -125,87 +144,100 @@ monogatari.script ({
 	],
 
 	'Scene1': [
-		'a Dahulu kala, terdapat seorang pelajar bernama Azri yang mendapati sukar untuk memahami bagaimana kurikulum di sekolah dapat membantu mereka mencapai kerjaya yang berjaya. ',
-		'Dahulu kala, terdapat seorang pelajar bernama Azri yang mendapati sukar untuk memahami bagaimana kurikulum di sekolah dapat membantu mereka mencapai kerjaya yang berjaya. ',
-		'Azri sering berasa frustrasi dan kurang motivasi dalam kelas-kelas mereka, dan sukar untuk melihat hubungan antara apa yang mereka pelajari dengan matlamat masa depan mereka.',
-		'Azri merupakan seorang yang kreatif dengan minat dalam seni. Masa lapang mereka diisi dengan menulis cerita, melukis kanvas berwarna-warni, dan menghasilkan melodinya sendiri dengan gitar.',
-		' Namun, apabila melibatkan subjek akademik seperti matematik dan sains, Azri tidak dapat menghilangkan rasa bahawa topik-topik ini tidak berkaitan dengan cita-cita seni mereka.',
-		
+		'Pada suatu ketika, terdapat seorang pelajar bernama Azri yang mendapati sukar untuk memahami bagaimana kurikulum di sekolah dapat membantu mereka mencapai kerjaya yang berjaya. ',
+		'Azri sering berasa stres dan kurang motivasi dalam kelasnya, dan sukar untuk melihat hubungan antara apa yang mereka pelajari dengan matlamat masa depan mereka.',
+		'Azri merupakan seorang yang kreatif dengan minat dalam seni. Masa lapangnya diisi dengan menulis cerita, melukis kanvas berwarna-warni, dan menghasilkan melodinya sendiri dengan gitar.',
+		' Namun, apabila melibatkan subjek akademik seperti matematik dan sains, Azri tidak dapat menghilangkan rasa bahawa topik-topik ini tidak berkaitan dengan cita-cita seni yang dimilikinya.',
+		'Pada malam itu, Azri melakukan soalan kerja sekolah di rumahnya.',
 		'jump QuestionScene1',// mula question1
 	],
 
 	'Scene2':[
-		'Pada suatu hari, Azri memutuskan untuk berbincang dengan kaunselor bimbingan mereka tentang kebimbangan mereka.',
-		'Kaunselor tersebut, Puan Thompson, terkenal dengan kebijaksanaan dan keupayaannya untuk membimbing pelajar ke arah potensi sebenar mereka. ',
-		'Dia mendengarkan dengan penuh perhatian ketika Azri menceritakan kekecewaan dan keraguan mereka.',
-		'Dengan senyuman lembut, Puan Thompson mula menjelaskan kepentingan pendidikan yang seimbang. ',
+		'Pada suatu hari, Azri memutuskan untuk berbincang dengan kaunselor sekolah tentang kebimbangannya.',
+		'show character a default at left with fadeIn',
+		'show character li default at right with fadeIn',
+		'Kaunselor tersebut, Puan Lisa, terkenal dengan kebijaksanaan dan keupayaannya untuk membimbing pelajar ke arah potensi sebenar mereka. ',
+		'Dia mendengarkan dengan penuh perhatian ketika Azri menceritakan kegelisahan dan keraguan mereka.',
+		'Dengan senyuman lembut, Puan Lisa mula menjelaskan kepentingan pendidikan yang seimbang. ',
 		'Dia berkongsi kisah-kisah seniman terkenal, penulis, dan pemuzik yang mencapai kejayaan bukan hanya melalui bakat kreatif mereka, tetapi juga melalui pengetahuan pelbagai subjek.',
-		'Dia menjelaskan bahawa walaupun mungkin tidak kelihatan jelas, kemahiran dan pengetahuan yang diperoleh melalui pendidikan adalah penting untuk kejayaan dalam apa jua kerjaya.',
-		'Untuk menggambarkan hujahnya, Puan Thompson menjemput Azri untuk menghadiri pameran kerjaya yang diadakan di sekolah mereka pada minggu berikutnya. ',
+		'Dia menjelaskan bahawa walaupun tidak memahami secara keseluruhan, kemahiran dan pengetahuan yang diperoleh melalui pendidikan adalah penting untuk kejayaan dalam apa jua kerjaya.',
+		'Untuk menggambarkan hujahnya, Puan Lisa menjemput Azri untuk menghadiri pameran kerjaya yang diadakan di sekolah mereka pada minggu berikutnya. ',
 		' Azri bersetuju, walaupun dengan skeptis, berharap untuk mendapatkan kejelasan dan inspirasi.',
+
+		'hide character li with fadeOut',
+		'show character a default at center with fadeIn',
+
+		'Azri keluar daripada bilik kaunseling dan terlanggar seorang murid yang membawa buku dalam keadaan terburu-buru',
+		'Azri menolongnya dan terlihat sebuah keratan kertas dan membacanya tanpa sengaja',
 		
 		'jump QuestionScene2',
 	],
 
 	'Scene3':[
-		'Di pameran kerjaya, Azri terkagum dengan pelbagai profesion yang diwakili.',
-		'Terdapat jurutera yang merancang teknologi terkini, doktor yang menyelamatkan nyawa, usahawan yang menjalankan perniagaan yang berjaya, dan juga ahli sains yang menyelidik penemuan baru. ',
-		' Ketika Azri meneroka kedai-kedai berbagai-bagai, mereka mendapat peluang untuk berbual dengan profesional dari bidang yang berbeza dan mempelajari perjalanan pendidikan mereka.',
+		'Di pameran kerjaya, Azri berasa kagum dengan pelbagai profesion yang dipamerkan.',
+		'Terdapat jurutera yang merancang teknologi terkini, doktor yang menyelamatkan nyawa, usahawan yang menjalankan perniagaan yang berjaya, dan juga ahli sains yang menyelidik penemuan baharu. ',
+		' Ketika Azri meneroka gerai-gerai yang dipamerkan, dia mendapat peluang untuk berbual dengan pakar dari bidang yang berbeza dan mempelajari perjalanan pendidikan mereka.',
+		'show character a default at left with fadeIn',
+		'show character e default at right with fadeIn',
+		
 		'Di salah satu kedai, Azri bertemu dengan seorang penulis terkenal bernama Emily, yang menceritakan kisah bagaimana cintanya pada kesusasteraan membawa dia untuk mengkaji Bahasa Inggeris di universiti.',
 		'Emily menekankan bahawa pengetahuannya tentang sejarah, psikologi, dan sosiologi memberinya pemahaman yang lebih mendalam tentang manusia, yang dia gunakan dalam naratifnya yang menarik.',
 		' Azri sedar bahawa walaupun dalam dunia seni, pendidikan yang seimbang memainkan peranan yang penting.',
+
+		'hide character e with fadeOut',
+		'show character a default at center with fadeIn',
+
+		'Azri membelek daripada brosur yang terdapat di pameran kerjaya dan melihat ...',
 	
 		'jump QuestionScene3',
 	],
 	'Scene4': [
-		'Di kedai yang lain, Azri bertemu dengan seorang arkitek bernama Marcus. ',
-		'Marcus menjelaskan bagaimana cintanya pada matematik dan fizik membimbingnya untuk mengejar ijazah dalam senibina. ',
+		'show character a default at left with fadeIn',
+		'show character aa default at right with fadeIn',
+		
+		'Di kedai yang lain, Azri bertemu dengan seorang arkitek bernama Aaron. ',
+		'Aaron menjelaskan bagaimana cintanya pada matematik dan fizik membimbingnya untuk mengejar ijazah dalam senibina. ',
 		'Dia menceritakan tentang pengiraan yang rumit dan kemahiran penyelesaian masalah yang diperlukan untuk merancang bangunan yang selamat dan berfungsi.',
-		'Azri melihat hubungan antara kepakaran teknikal Marcus dengan keindahan artistik ciptaan arsitekturnya.',
+		'Azri melihat hubungan antara kepakaran teknikal Aaron dengan keindahan artistik ciptaan arsitekturnya.',
+
+		'hide character aa with fadeOut',
+		'show character a default at center with fadeIn',
+
 		'Saat Azri menjelajahi pameran kerjaya, mereka mendengar banyak kisah profesional yang telah menggunakan latar belakang pendidikan yang berbeza untuk berkembang dalam bidang yang mereka pilih. ',
 		'Keraguan Azri mulai menghilang, digantikan dengan penghargaan baru terhadap kepentingan pendidikan.',
-	
+
+		'Sewaktu Azri berjalan pulang daripada pameran kerjaya, dia masih terngiang-ngiang terhadap satu pelajaran yang dia pelajari',
 		'jump QuestionScene4',
 	],
 	
 	'Scene5':[
 		'Bersenjatakan perspektif baru ini, Azri kembali ke kelas-kelas mereka dengan semangat yang baru.',
 		'Mereka mulai melihat nilai dalam setiap subjek dan bekerja keras untuk berjaya dalam kelas-kelas mereka. ',
-		'Azri menyedari bahawa walaupun subjek seperti matematik dan sains, yang mungkin tidak berkaitan dengan impian seni mereka, mampu memupuk kemahiran seperti pemikiran kritis, penyelesaian masalah, dan komunikasi yang efektif yang bernilai dalam apa jua kerjaya.',
+		'Azri menyedari bahawa walaupun subjek seperti matematik dan sains, yang mungkin tidak berkaitan dengan impian seni mereka, setidaknya ia mampu kemahiran seperti pemikiran kritis, penyelesaian masalah, dan komunikasi yang efektif yang bernilai dalam apa jua kerjaya.',
 		
+		'Semasa didalam kelas, Cikgu ada bertanya kepada kelasnya,',
+		'Azri mencuba nasibnya untuk menjawab setelah dia mendapat semangat yang baharu',
 		'jump QuestionScene5',	
 	],
 
 	'Scene6':[
-		'Tahun-tahun berlalu, dan Azri tamat belajar dari sekolah dengan kepujian.',
-		' Dedikasi dan usaha keras mereka berbalas, dan mereka diterima di kolej seni yang terkenal.',
+		'Setelah 2 tahun berlalu, dan Azri tamat belajar dari sekolah dengan kepujian.',
+		' Dedikasi dan usaha keras mereka berbalas, dan mereka diterima di sebuah kolej seni yang terkenal.',
 		'Di sana, Azri terus memupuk bakat kreatif mereka sambil mengambil manfaat dari pengetahuan yang mereka peroleh melalui pendidikan yang seimbang.',
-		'Saat Azri menjalani tahun-tahun koleja, mereka mula menggabungkan cinta mereka pada seni dengan pengetahuan mereka tentang subjek-subjek lain. ',
-		'Mereka mencipta instalasi yang membangkitkan pemikiran terinspirasi oleh sains, menggabungkan prinsip-prinsip matematik ke dalam lukisan abstrak mereka, dan bahkan menulis cerita yang menjelajahi isu-isu sosial dan peristiwa sejarah.',
+		'Saat Azri menjalani tahun-tahun pengajian di kolej, mereka mula menggabungkan cintanya pada seni dengan pengetahuan dia tentang subjek-subjek lain. ',
+		'Beliau mencipta hasil seni yang membangkitkan pemikiran terinspirasi oleh sains, menggabungkan prinsip-prinsip matematik ke dalam lukisan abstrak mereka, dan bahkan menulis cerita yang menjelajahi isu-isu sosial dan peristiwa sejarah.',
 		'Pendekatan Azri yang unik dan antardisiplin membawa perhatian peminat seni, pengkritik, dan profesional. Karya mereka melampaui batas tradisional, memukau penonton dan mendapatkan pengiktirafan di dunia seni. ',
 		'Kejayaan Azri berkembang, dan mereka menjadi seorang seniman terkenal yang dikenali dengan kemampuan mereka untuk menggabungkan kreativiti dan pengetahuan dengan harmoni.',
-		'Mengingat kembali, Azri sedar bahawa keraguan dan kekecewaan awal mereka tentang pendidikan telah berubah menjadi penghargaan mendalam terhadap kekuatan pembelajaran.',
-		'Mereka memahami bahawa pendidikan bukan sekadar alat untuk mencapai tujuan tetapi perjalanan sepanjang hayat yang dapat membentuk perspektif, meluaskan horizons, dan menyuburkan usaha seni mereka.',
+		'Mengingat kembali, Azri sedar bahawa keraguan dan kekecewaan awal mereka tentang pendidikan telah berubah menjadi penghargaan mendalam terhadap penghasilan kerjayanya.',
+		'Mereka memahami bahawa pendidikan bukan sekadar alat untuk mencapai tujuan tetapi perjalanan sepanjang hayat yang dapat membentuk perspektif, meluaskan batas pemikiran, dan menyuburkan usaha seni mereka.',
 		'Sejak saat itu, Azri tidak pernah meremehkan kepentingan pendidikan dan kesan yang dapat dimilikinya dalam mencapai kerjaya yang berjaya dan memuaskan. ',
 		'Mereka menjadi penyokong integrasi disiplin yang berbeza dan mendorong orang lain untuk menghayati kekuatan pendidikan yang seimbang.',
 		'Dan begitulah, kisah Azri menjadi sumber inspirasi bagi banyak pelajar yang, seperti mereka, pernah mempertanyakan nilai pelajaran mereka.',
-		' Ketika mereka terus mencipta, berinovasi, dan mendorong batas-batas seni, warisan Azri hidup, mengingatkan semua orang bahwa kejayaan sejati tidak hanya datang dari bakat tetapi juga dari pengetahuan dan kemahiran yang diperoleh melalui pendidikan.',
+		' Ketika mereka terus mencipta, berinovasi, dan mendorong batas-batas seni, warisan Azri terus hidup, mengingatkan semua orang bahwa kejayaan sejati tidak hanya datang dari bakat tetapi juga dari pengetahuan dan kemahiran yang diperoleh melalui pendidikan.',
 		
 		'The end.',
 		'end'
 	],
-	
-	
-
-	// 'altScene': [
-
-	// 	// 'nvl This is an pov 3rd person side me.',
-	// 	// 'nvl Here is an example of NVL text.',
-	// 	// 'nvl Here is some more NVL text.',
-	// 	// 'nvl One more line.'
-		
-	// ],
-	
 	
 	
 	'QuestionScene1': [
@@ -216,7 +248,7 @@ monogatari.script ({
 		{"Choice":{
 			"FirstOption":{
 				"Text": "A. Kurikulum kebangsaan",
-				"Do": "jump labelBetul1",
+				"Do": "jump labelSalah1",
 				
 		},
 			"SecondOption":{
@@ -231,7 +263,7 @@ monogatari.script ({
 			},
 			"FourthOption":{
 				"Text": "D. Falsafah Pendidikan Kebangsaan",
-				"Do": "jump labelSalah1",
+				"Do": "jump labelBetul1",
 				
 			}
 		}},
@@ -298,7 +330,7 @@ monogatari.script ({
 		{"Choice":{
 			"FirstOption":{
 				"Text": "A. Kurikulum ideal.",
-				"Do": "jump labelBetul3",
+				"Do": "jump labelSalah3",
 				
 		},
 			"SecondOption":{
@@ -313,7 +345,7 @@ monogatari.script ({
 			},
 			"FourthOption":{
 				"Text": "D. Kurikulum tersembunyi.",
-				"Do": "jump labelSalah3",
+				"Do": "jump labelBetul3",
 				
 			}
 		}},
@@ -380,12 +412,12 @@ monogatari.script ({
 		{"Choice":{
 			"FirstOption":{
 				"Text": "A. I, Il dan III sahaja.",
-				"Do": "jump labelBetul5",
+				"Do": "jump labelSalah5",
 				
 		},
 			"SecondOption":{
 				"Text": "B. I, II dan IV sahaja.",
-				"Do": "jump labelSalah5",
+				"Do": "jump labelBetul5",
 				
 			},
 			"ThirdOption":{
